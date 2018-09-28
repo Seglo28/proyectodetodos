d0a9c573dc61f352e8c300fb96f67778466f2692
delmy.perezusam <delmy.perezusam@CDS2-USAM047.sv.cds>
2018-09-28 14:14:38 -0600

    memi: archivo de ventas  y activación funcionales.

--- InventarioStruts/src/java/action/ActionVentas.java
+++ InventarioStruts/src/java/action/ActionVentas.java
@@ -32,6 +32,7 @@ public class ActionVentas extends org.apache.struts.action.Action {
 
     private static final String agregarVEN = "agregarVEN";
     private static final String consultarVEN = "consultarVEN";
+     private static final String todasVEN = "todasVEN";
     private static final String consultarIdVEN = "consultarIdVEN";
     private static final String modificarVEN = "modificarVEN";
     private static final String borrarVEN = "borrarVEN";
@@ -55,13 +56,15 @@ public class ActionVentas extends org.apache.struts.action.Action {
         Integer idSucursal = af.getIdSucursal();
         Integer cantidad = af.getCantidad();
         Double monto = af.getMonto();
-        String nDocumento = af.getnDocumento();
+        String nDocumento = af.getNDocumento();
         String fechaVenta = af.getFechaVenta();
+        String estadoVenta = af.getEstadoVenta();
         String action = af.getAction();
         String mensaje = af.getMensaje();
 
         if (af == null || action == null) {
-            af.setError("<div class = 'alert alert-danger'> Esta ocurriendo un problema</div>");
+            af.setError("Esta ocurriendo un problema");           
+             request.setAttribute("info", mensaje);
             return map.findForward(errorVEN);
         }
 
@@ -72,16 +75,18 @@ public class ActionVentas extends org.apache.struts.action.Action {
             String adver7 = "";
 
             if (cantidad == null || cantidad.equals("") || cantidad == 0) {
-                adver5 = "*Ingrese la cantidad de producto deseado.";
+                adver5 = "- Ingrese la cantidad .";
             }
             if (monto == null || monto.equals("") || monto == 0) {
-                adver6 = "Ingrese el monto total.";
+                adver6 = "- Ingrese el monto total.";
             }
             if (nDocumento == null || nDocumento.equals("")) {
-                adver7 = "Ingrese el  numero de serie del documento total.";
+                adver7 = "- Ingrese el  numero de serie del documento.";
             }
+            
+            System.out.println("Adver7 "+adver7);
 
-            if (!adver.equals("")) {
+            if (!adver7.equals("")) {
                 ClienteMantenimiento cm = new ClienteMantenimiento();
                 List<Clientes> listaCli = cm.consultarTodosClientes();
                 af.setListaCli(listaCli);
@@ -102,10 +107,11 @@ public class ActionVentas extends org.apache.struts.action.Action {
                 af.setListaSuc(listaSuc);
                 request.setAttribute("listasSuc", listaSuc);
 
-                mensaje = ("<div class='alert alert-danger'>Debe completar todos los requerimientos. <br>" + adver5 + adver6 + adver7 + "</div>");
+                mensaje = "Debe completar todos los requerimientos. " + adver5 + adver6 + adver7 + "";
                 request.setAttribute("error", mensaje);
-                return map.findForward(irFormVenta);
+                return map.findForward(errorVEN);
             }
+            
             String fechaHoy = formatoFecha.format(new Date());
             System.out.println("Fecha:" + fechaHoy);
             VentasMantenimiento ven = new VentasMantenimiento();
@@ -117,10 +123,64 @@ public class ActionVentas extends org.apache.struts.action.Action {
             System.out.println("nDocumento:" + nDocumento);
             System.out.println("cantidad:" + cantidad);
             System.out.println("monto: $" + monto);
+            
+            Ventas val = ven.valNDoc(nDocumento);
+            
+            if (val!=null) {
+                ClienteMantenimiento cm = new ClienteMantenimiento();
+                List<Clientes> listaCli = cm.consultarTodosClientes();
+                af.setListaCli(listaCli);
+                request.setAttribute("listaCli", listaCli);
 
+                ProductosMantenimiento mprod = new ProductosMantenimiento();
+                List<Productos> listaProd = mprod.consultarTodosProductos();
+                af.setListaPro(listaProd);
+                request.setAttribute("listaProd", listaProd);
+
+                MantenimientoUsuario mu = new MantenimientoUsuario();
+                List<Usuario> listaUsu = mu.consultarTodosUsuarios();
+                af.setListaUsu(listaUsu);
+                request.setAttribute("listaUsu", listaUsu);
+
+                SucursalesMantenimiento scm = new SucursalesMantenimiento();
+                List<Sucursales> listaSuc = scm.consultarTodosSucursales();
+                af.setListaSuc(listaSuc);
+                request.setAttribute("listasSuc", listaSuc);
+
+                mensaje = "El n° de factura de venta ya fue registrado";
+                request.setAttribute("info", mensaje);
+                return map.findForward(errorVEN);
+                
+            }
+           // --- extrayendo de Inventario  ---
             InventarioMantenimiento minv = new InventarioMantenimiento();
-            Inventario inv = minv.consultarInventarioProducto(idProducto, idSucursal);
-            System.out.println("inv: " + inv);
+            Inventario inv = minv.consultarInventarioProducto(idProducto, idSucursal); 
+         
+            if(inv == null){
+                ClienteMantenimiento cm = new ClienteMantenimiento();
+                List<Clientes> listaCli = cm.consultarTodosClientes();
+                af.setListaCli(listaCli);
+                request.setAttribute("listaCli", listaCli);
+
+                ProductosMantenimiento mprod = new ProductosMantenimiento();
+                List<Productos> listaProd = mprod.consultarTodosProductos();
+                af.setListaPro(listaProd);
+                request.setAttribute("listaProd", listaProd);
+
+                MantenimientoUsuario mu = new MantenimientoUsuario();
+                List<Usuario> listaUsu = mu.consultarTodosUsuarios();
+                af.setListaUsu(listaUsu);
+                request.setAttribute("listaUsu", listaUsu);
+
+                SucursalesMantenimiento scm = new SucursalesMantenimiento();
+                List<Sucursales> listaSuc = scm.consultarTodosSucursales();
+                af.setListaSuc(listaSuc);
+                request.setAttribute("listasSuc", listaSuc);
+                
+                mensaje = ("No Existe inventario de este producto");
+                request.setAttribute("warning", mensaje);
+                return map.findForward(errorVEN);
+            }
 
             Integer idInv = inv.getIdInventario();
 
@@ -196,21 +256,53 @@ public class ActionVentas extends org.apache.struts.action.Action {
             VentasMantenimiento ven = new VentasMantenimiento();
             List<Ventas> listaVEN = ven.consultarTodosVentas();
             System.out.println("ESTA ES SU LISTA: " + listaVEN);
-            if (listaVEN == null) {
-                af.setError("<div class='alert alert-danger'>No hay Datos Guardados en la base de datos sobre ventas.</div>");
-                return map.findForward(consultarVEN);
+            if (listaVEN.isEmpty()) {
+                mensaje="No se ha reizado ninguna  venta en la sucursal";
+                request.setAttribute("info", mensaje);
             } else {
                 af.setListaVen(listaVEN);
-                return map.findForward(consultarVEN);
+                
             }
+            return map.findForward(consultarVEN);
         }
+        
+        if (action.equals("Cancelar")) {
+            VentasMantenimiento ven = new VentasMantenimiento();
+            List<Ventas> listaVEN = ven.consultarTodosVentas();
+            System.out.println("ESTA ES SU LISTA: " + listaVEN);
+            if (listaVEN.isEmpty()) {
+                mensaje="No se ha reizado ninguna  venta en la sucursal";
+                request.setAttribute("info", mensaje);
+            } else {
+                af.setListaVen(listaVEN);
+               return map.findForward(consultarVEN);   
+            }
+          
+        }
+        
+          if (action.equals("Archivo Ventas")) {
+            VentasMantenimiento ven = new VentasMantenimiento();
+            List<Ventas>listaVEN = ven.consultarVentasArchivadas();
+            
+            if (listaVEN.isEmpty()) {
+                mensaje = "No Se ha archivado ninguna Venta Aún";
+                request.setAttribute("info", mensaje);
+            } else {
+                af.setListaVen(listaVEN);
+            }
+            return map.findForward(todasVEN);
+        }
+        
+          
+          
 
         if (action.equals("Actualizar")) {
             VentasMantenimiento vem = new VentasMantenimiento();
             Ventas ven = vem.consultarVentas(idVenta);
 
             if (ven == null) {
-                af.setError("<div class='alert alert-danger'> No se puede realizar la consulta para actualizar la venta..</div>");
+                mensaje = "Hay un problema en el Sistema!(action ventas)";
+                request.setAttribute("error", mensaje);
                 return map.findForward(errorVEN);
             } else {
                 System.out.println("ID:" + ven.getIdVenta());
@@ -259,74 +351,156 @@ public class ActionVentas extends org.apache.struts.action.Action {
             System.out.println("cantidad:" + cantidad);
             System.out.println("monto: $" + monto);
             System.out.println("Fecha:" + fechaVenta);
-            int vent = vem.ActualizarVenta(idVenta, idCliente, idInventario, idProducto, idUsuario, idSucursal, cantidad, monto, fechaVenta);
-            List<Ventas> listaVEN = vem.consultarTodosVentas();
+            
+            int vent = vem.ActualizarVenta(idVenta, idCliente, idInventario, nDocumento, idProducto, idUsuario, idSucursal, cantidad, monto, fechaVenta, estadoVenta);
+            if(vent == 1){
+                mensaje = "La venta se ha modificado con Éxito!";
+                request.setAttribute("mensaje", mensaje);
+            } else {
+                mensaje = "Error al modificar la venta!";
+                request.setAttribute("error", mensaje);
+            }
+            
+            List<Ventas> listaVEN = vem.consultarVentasDisponibles();
             af.setListaVen(listaVEN);
             return map.findForward(consultarVEN);
-
         }
+        if (action.equals("Archivar")) {
+            VentasMantenimiento vem = new VentasMantenimiento();
+            int key = Integer.parseInt(request.getParameter("id"));
+            
+            System.out.println("Id: "+key);
+            
+            Ventas arch = vem.consultarVentas(key);
+            System.out.println("Cons: "+arch);
+            String nD = arch.getNDocumento();
+            int idCli=arch.getClientes().getIdCliente();
+            int idPro = arch.getProductos().getIdProducto();
+            int idSuc=arch.getSucursales().getIdSucursal();
+            int idUs=arch.getUsuario().getIdUsuario();
+            int idInv = arch.getInventario().getIdInventario();
+            int cant = arch.getCantidad();
+            double mont = arch.getMonto();
+            String fechaVen = arch.getFechaVenta();
+            System.out.println("Inv: "+idInv);
+            int r = vem.archivarVenta(key, idCli, idInv, nD, idPro, idUs, idSuc, cant, mont, fechaVen);
+            
+            if(r == 1){
+                mensaje = "Se ha archivado con Éxito!";
+                request.setAttribute("mensaje", mensaje);
+            } else {
+                mensaje = "Error al archivar!";
+                request.setAttribute("error", mensaje);
+            }
+            
+            List<Ventas> listaVEN = vem.consultarVentasDisponibles();
+            
+            if (listaVEN.isEmpty()) {
+                mensaje = "No Se ha realizado ninguna Venta Aún";
+                request.setAttribute("info", mensaje);
+            } else {
+                af.setListaVen(listaVEN);
+            }
+            return map.findForward(consultarVEN);
+        }
+        
+         if (action.equals("Activar")) {
+            VentasMantenimiento vem = new VentasMantenimiento();
+            int key = Integer.parseInt(request.getParameter("id"));
+            
+            Ventas arch = vem.consultarVentas(key);
+            String nD = arch.getNDocumento();
+            int idCli=arch.getClientes().getIdCliente();
+            int idPro = arch.getProductos().getIdProducto();
+            int idSuc=arch.getSucursales().getIdSucursal();
+            int idUs=arch.getUsuario().getIdUsuario();
+            int idInv=arch.getInventario().getIdInventario();
+            int cant = arch.getCantidad();
+            double mont = arch.getMonto();
+            
+            String fechaVen = arch.getFechaVenta();
+            int r =vem.activarVenta(key, idCli, idInv, nD, idPro, idUs, idSuc, cant, mont, fechaVen);
+                    
+            
+            if(r == 1){
+                mensaje = "Se ha activado con Éxito!";
+                request.setAttribute("mensaje", mensaje);
+            } else {
+                mensaje = "Error al activar!";
+                request.setAttribute("error", mensaje);
+            }
+            
+            List<Ventas> listaVEN = vem.consultarVentasArchivadas();
+            
+            if (listaVEN.isEmpty()) {
+                mensaje = "No Se ha activado ninguna venta Aún";
+                request.setAttribute("info", mensaje);
+            } else {
+                af.setListaVen(listaVEN);
+            }
+            return map.findForward(todasVEN);
+        }
+        
 
         if (action.equals("Borrar")) {
             VentasMantenimiento venMan = new VentasMantenimiento();
             venMan.eliminarVentas(idVenta);
-            af.setError(borrarVEN);
+           af.setError(borrarVEN);
             List<Ventas> listaVen = venMan.consultarTodosVentas();
-            af.setListaVen(listaVen);
+           af.setListaVen(listaVen);
             return map.findForward(borrarVEN);
-        }
+       }
 
-        if (action.equals("Eliminar")) {
+      if (action.equals("Eliminar")) {
             VentasMantenimiento ven = new VentasMantenimiento();
             Ventas ventas = ven.consultarVentas(idVenta);
             if (ventas == null) {
-                af.setError("<div class='alert alert-danger'>No se puede realizar la consulta</div>");
-                return map.findForward(errorVEN);
-            } else {
-                af.setIdVenta(ventas.getIdVenta());
-                af.setIdCliente(ventas.getClientes().getIdCliente());
-                af.setIdProducto(ventas.getProductos().getIdProducto());
-                af.setIdUsuario(ventas.getUsuario().getIdUsuario());
-                af.setIdSucursal(ventas.getSucursales().getIdSucursal());
-                af.setCantidad(ventas.getCantidad());
-                af.setMonto(ventas.getMonto());
-                af.setFechaVenta(ventas.getFechaVenta());
+                 return map.findForward(errorVEN);
+           } else {
+               af.setIdVenta(ventas.getIdVenta());
+               af.setIdCliente(ventas.getClientes().getIdCliente());
+               af.setIdProducto(ventas.getProductos().getIdProducto());
+               af.setIdUsuario(ventas.getUsuario().getIdUsuario());
+               af.setCantidad(ventas.getCantidad());
+              af.setMonto(ventas.getMonto());
+              af.setFechaVenta(ventas.getFechaVenta());
 
-                ClienteMantenimiento cliMan = new ClienteMantenimiento();
+               ClienteMantenimiento cliMan = new ClienteMantenimiento();
                 List<Clientes> listaCli = cliMan.consultarTodosClientes();
                 af.setListaCli(listaCli);
-                request.setAttribute("listaCli", listaCli);
+               request.setAttribute("listaCli", listaCli);
 
                 ProductosMantenimiento proMan = new ProductosMantenimiento();
                 List<Productos> listaPro = proMan.consultarTodosProductos();
                 af.setListaPro(listaPro);
-                request.setAttribute("listaPro", listaPro);
+               request.setAttribute("listaPro", listaPro);
 
                 MantenimientoUsuario usuMan = new MantenimientoUsuario();
-                List<Usuario> listaUsu = usuMan.consultarTodosUsuarios();
+               List<Usuario> listaUsu = usuMan.consultarTodosUsuarios();
                 af.setListaUsu(listaUsu);
-                request.setAttribute("listaUsu", listaUsu);
+               request.setAttribute("listaUsu", listaUsu);
 
-                SucursalesMantenimiento sucMan = new SucursalesMantenimiento();
-                List<Sucursales> listaSuc = sucMan.consultarTodosSucursales();
+               SucursalesMantenimiento sucMan = new SucursalesMantenimiento();
+              List<Sucursales> listaSuc = sucMan.consultarTodosSucursales();
                 af.setListaSuc(listaSuc);
-                request.setAttribute("listaSuc", listaSuc);
+               request.setAttribute("listaSuc", listaSuc);
 
-                return map.findForward(eliminarVEN);
-            }
-        }
+              return map.findForward(eliminarVEN);
+           }
+       }
 
-        if (action.equals("Volver")) {
-            VentasMantenimiento ven = new VentasMantenimiento();
-            List<Ventas> listaVEN = ven.consultarTodosVentas();
-            System.out.println("ESTA ES SU LISTA: " + listaVEN);
-            if (listaVEN == null) {
-                af.setError("<div class='alert alert-danger'>No hay Datos Guardados en la base de datos sobre ventas.</div>");
-                return map.findForward(consultarVEN);
-            } else {
-                af.setListaVen(listaVEN);
-                return map.findForward(consultarVEN);
-            }
+       if (action.equals("Volver")) {
+           VentasMantenimiento ven = new VentasMantenimiento();
+          List<Ventas> listaVEN = ven.consultarTodosVentas();
+           System.out.println("ESTA ES SU LISTA: " + listaVEN);
+           if (listaVEN == null) {
+              af.setError("<div class='alert alert-danger'>No hay Datos Guardados en la base de datos sobre ventas.</div>");
+               return map.findForward(consultarVEN);
+           } else {
+              af.setListaVen(listaVEN);
+              return map.findForward(consultarVEN);
         }
+      }
             return null;
         }
     }
